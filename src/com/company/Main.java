956048e8c49f1a3d4465befb5fb0f0e76a618864
Brian <11246170@student.uts.edu.au>
2018-08-15 18:50:36 +1000

    updated style checking

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,13 +13,15 @@ import java.util.List;
 
 /**
  *
+ * http://webapp.docx4java.org/OnlineDemo/ecma376/WordML/index.html
+ *
  * https://www.experts-exchange.com/questions/29054970/How-to-get-italic-text-using-docx4j-in-java.html
+ * https://github.com/plutext/docx4j/blob/master/src/main/java/org/docx4j/model/PropertyResolver.java
  *
  * **/
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         BasicConfigurator.configure();
 
         //JFileChooser jfc = new JFileChooser();
@@ -33,21 +35,26 @@ public class Main {
             Document wmlDocEle = docPart.getJaxbElement();
             Body body = wmlDocEle.getBody();
             List<Object> paraList = TraversalUtil.getChildrenImpl(body);
-            //List<Object> paraWRList = TraversalUtil.getChildrenImpl(paraList.get(1));
 
             for (Object a : paraList) {
                 List<Object> paraWRList = TraversalUtil.getChildrenImpl(a);
-                for (Object b : paraWRList)
-                    if (b != null) {
-                        if (((R) XmlUtils.unwrap(b)).getRPr() != null) {
-                            if (((R) XmlUtils.unwrap(b)).getRPr().getB() != null) {
+                if (paraWRList != null) {
+                    for (Object b : paraWRList) {
+                        if (b != null) {
+                            if (b.getClass().getName().equalsIgnoreCase("org.docx4j.wml.R")) {
+                                if (((R) XmlUtils.unwrap(b)).getRPr() != null) {
+                            /*if (((R) XmlUtils.unwrap(b)).getRPr().getB() != null) {
                                 //System.out.println( + ": bold");
                                 System.out.println(a.toString() + ": bold");
                             } else {
                                 System.out.println(a.toString() + ": not bold");
+                            }*/
+                                    styleCheck(a.toString(),((R) XmlUtils.unwrap(b)).getRPr());
+                                }
                             }
                         }
                     }
+                }
             }
                 /*for (int i = 0; i < obj.size(); i++) {
                     obj.get(i);
@@ -58,6 +65,71 @@ public class Main {
             //}
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void styleCheck (String s,RPr rpr) {
+        if (rpr != null) {
+
+            System.out.println("**************************************************");
+            System.out.println(s);
+
+            //Font
+            if (rpr.getRFonts()!=null ) {
+                System.out.println(rpr.getRFonts().toString());
+            }
+
+            //Bold
+            if (rpr.getB()!=null) {
+                System.out.println("Bold");
+            }
+
+            //Italics
+            if (rpr.getI()!=null) {
+                System.out.println("Italics");
+            }
+
+            //Caps
+            if (rpr.getCaps()!=null) {
+                System.out.println("Caps");
+            }
+
+            //Small Caps
+            if (rpr.getSmallCaps()!=null) {
+                System.out.println("Small Caps");
+            }
+
+            //Strikethrough
+            if (rpr.getStrike()!=null) {
+                System.out.println("Strike");
+            }
+
+            //Font Colour
+            if (rpr.getColor()!=null ) {
+                System.out.println("Coloured Font");
+            }
+
+            //Size
+            if (rpr.getSz()!=null ) {
+                System.out.println("Font Size");
+            }
+
+            //Underline;
+            if (rpr.getU()!=null ) {
+                System.out.println("Underline");
+            }
+
+            //Text Border;
+            if (rpr.getBdr()!=null ) {
+                System.out.println("Border");
+            }
+
+            //Shading;
+            if (rpr.getShd()!=null ) {
+                System.out.println("Shading");
+            }
+
+            System.out.println("**************************************************");
         }
     }
 }
